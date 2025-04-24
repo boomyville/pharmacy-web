af1fb0344d88ca0755251f2418fd5f7b9fc660cc
Kevin Teong <boomyville@gmail.com>
2025-04-24 19:25:57 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    if (this.hash) {
+      e.preventDefault();
+      document.querySelector(this.hash).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
